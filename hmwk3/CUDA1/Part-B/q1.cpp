c627e9d99c5a820d45f21c4371499d3112d1e632
Denzel Farmer <denzelgfarmer@gmail.com>
2024-11-09 23:35:51 +0000

    Add Bq1 and b Q2

---
+++ hmwk3/CUDA1/Part-B/q1.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <sstream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+struct alloc_sum_result {
+    microseconds alloc_time;
+    microseconds populate_time;
+    microseconds calc_time;
+    microseconds total_time;
+    // Keep the last sum value and print it out to avoid optimizer skipping 
+    float last_sum;
+};
+
+#define EPS ((float)1e-4)
+
+bool verify_sum_arrays(size_t count, float *input1, float *input2, float *output) {
+    for (size_t i = 0; i < count; i++) {
+        float exp_sum = input1[i] + input2[i];
+        if (abs(output[i] - exp_sum) > EPS)
+            return false;
+    }
+
+    return true;
+
+}
+
+void sum_arrays(size_t count, float *input1, float *input2, float *output) {
+    for (size_t i = 0; i < count; i ++){
+        output[i] = input1[i] + input2[i];
+    }
+}
+
+alloc_sum_result time_sum_arrays(size_t count) {
+    alloc_sum_result result;
+    result.alloc_time = microseconds(-1);
+    result.populate_time = microseconds(-1);
+    result.calc_time = microseconds(-1);
+    result.total_time = microseconds(-1);
+    auto t0 = high_resolution_clock::now();
+    
+    float *array1, *array2, *out;
+    array1 = (float *)malloc(count * sizeof(float));
+    if (!array1)
+        return result;
+
+    array2 = (float *)malloc(count * sizeof(float));
+    if (!array2){
+        free(array1);
+        return result;
+    }
+
+    out = (float *)malloc(count * sizeof(float));
+    if (!out){
+        free(array1);
+        free(array2);
+        return result;
+    }
+    
+    auto t1 = high_resolution_clock::now();
+    result.alloc_time = duration_cast<microseconds>(t1 - t0);
+
+    for (size_t i = 0; i < count; i++) {
+        array1[i] = (float) i;
+        array2[i] = (float) i;
+    }
+
+    auto t2 = high_resolution_clock::now();
+    result.populate_time = duration_cast<microseconds>(t2 - t1);
+
+    sum_arrays(count, array1, array2, out);
+
+    auto t3 = high_resolution_clock::now();
+    result.calc_time = duration_cast<microseconds>(t3-t2);
+
+    // This is a bit pointless here
+    verify_sum_arrays(count, array1, array2, out);
+
+    result.last_sum = out[count-1];
+
+    free(array1);
+    free(array2);
+    free(out);
+
+    auto t4 = high_resolution_clock::now();
+    result.total_time = duration_cast<microseconds>(t4-t0);
+    return result;
+}
+
+int main(int const argc, char *argv[]) {
+
+    if (argc != 2){
+        cout << "Usage: " << argv[0] << " ELEMS\n";
+        exit(1);
+    }
+
+    size_t elems;
+    stringstream sstream(argv[1]);
+    sstream >> elems;
+    if (elems == 0) {
+        cout << "Invalid element size\n";
+        exit(1);
+    }
+
+    cout << "Using array of size: " << elems << "M" << endl;
+    
+    auto result = time_sum_arrays(elems*(1e6));
+    cout << "Total Time: " << duration_cast<milliseconds>(result.total_time).count() << "ms" << endl;
+    cout << "Allocation Time: " << duration_cast<milliseconds>(result.alloc_time).count() << "ms" << endl;
+    cout << "Populate Time: " << duration_cast<milliseconds>(result.populate_time).count() << "ms" << endl;
+    cout << "Calculation Time: " << duration_cast<milliseconds>(result.calc_time).count() << "ms" << endl;
+    cout << "Total Time: " << duration_cast<milliseconds>(result.total_time).count() << "ms" << endl;
+    cout << "Last Sum Value: " << result.last_sum << endl;
+}
---
+++ hmwk3/CUDA1/Part-B/q2.cu
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <sstream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+struct alloc_sum_result {
+    microseconds alloc_time;
+    microseconds populate_time;
+    microseconds calc_time;
+    microseconds total_time;
+    // Keep the last sum value and print it out to avoid optimizer skipping 
+    float last_sum;
+};
+
+#define EPS ((float)1e-4)
+
+bool verify_sum_arrays(size_t count, float *input1, float *input2, float *output) {
+    for (size_t i = 0; i < count; i++) {
+        float exp_sum = input1[i] + input2[i];
+        if (abs(output[i] - exp_sum) > EPS) {
+            cout << "Mismatch at index " << i << ": expected " << exp_sum << ", got " << output[i] << endl;
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+__global__ void SumArrays(const float* A, const float* B, float* C, int N)
+{
+    // number of threads accessing together 
+    int stride_size = (blockDim.x)*(gridDim.x);
+    int blockStartIndex  = blockIdx.x * blockDim.x;
+    int threadStartIndex = blockStartIndex + threadIdx.x;
+
+    int thread_id = (blockIdx.x)*(blockDim.x) + (threadIdx.x);
+
+    int i, curr_index;
+    
+    for (i=0; i < N; i++) {
+        curr_index = threadStartIndex + i*stride_size;
+        C[curr_index] = A[curr_index] + B[curr_index];
+    }
+    
+}
+
+
+float *h_array1, *d_array1, *h_array2, *d_array2, *h_out, *d_out;
+
+void Cleanup(bool noError) {  // simplified version from CUDA SDK
+    cudaError_t error;
+    // Free device vectors
+    if (d_array1)
+        cudaFree(d_array1);
+    if (d_array2)
+        cudaFree(d_array2);
+    if (d_out)
+        cudaFree(d_out);
+
+    // Free host memory
+    if (h_array1)
+        free(h_array1);
+    if (h_array2)
+        free(h_array2);
+    if (h_out)
+        free(h_out);
+    error = cudaDeviceReset();
+    
+    if (!noError || error != cudaSuccess)
+        printf("cuda malloc or cuda thread exit failed \n");
+    
+    fflush( stdout);
+    fflush( stderr);
+
+    exit(0);
+}
+
+void checkCUDAError(const char *msg)
+{
+  cudaError_t err = cudaGetLastError();
+  if( cudaSuccess != err) 
+    {
+      fprintf(stderr, "Cuda error: %s: %s.\n", msg, cudaGetErrorString(err) );
+      exit(-1);
+    }                         
+}
+
+
+alloc_sum_result time_sum_arrays(size_t count, size_t blocks, size_t threads) {
+    alloc_sum_result result;
+    result.alloc_time = microseconds(-1);
+    result.populate_time = microseconds(-1);
+    result.calc_time = microseconds(-1);
+    result.total_time = microseconds(-1);
+    auto t0 = high_resolution_clock::now();
+    
+    h_array1 = (float *)malloc(count * sizeof(float));
+    if (!h_array1) Cleanup(false);
+    h_array2 = (float *)malloc(count * sizeof(float));
+    if (!h_array2) Cleanup(false);
+    h_out = (float *)malloc(count * sizeof(float));
+    if (!h_out) Cleanup(false);
+
+    cudaError_t error;
+    error = cudaMalloc((void**)&d_array1, count * sizeof(float));
+    if (error != cudaSuccess) Cleanup(false);
+    error = cudaMalloc((void**)&d_array2, count * sizeof(float));
+    if (error != cudaSuccess) Cleanup(false);
+    error = cudaMalloc((void**)&d_out, count * sizeof(float));
+    if (error != cudaSuccess) Cleanup(false);
+
+    auto t1 = high_resolution_clock::now();
+    result.alloc_time = duration_cast<microseconds>(t1 - t0);
+
+    for (size_t i = 0; i < count; i++) {
+        h_array1[i] = (float) i;
+        h_array2[i] = (float) count - i;
+    }
+
+    // Copy to device 
+    error = cudaMemcpy(d_array1, h_array1, count*sizeof(float), cudaMemcpyHostToDevice);
+    if (error != cudaSuccess) Cleanup(false);
+    error = cudaMemcpy(d_array2, h_array2, count*sizeof(float), cudaMemcpyHostToDevice);
+    if (error != cudaSuccess) Cleanup(false);
+
+    auto t2 = high_resolution_clock::now();
+    result.populate_time = duration_cast<microseconds>(t2 - t1);
+
+    dim3 dimGrid(blocks);                    
+    dim3 dimBlock(threads);       
+
+    size_t values_per_thread = (count) / (blocks * threads);
+
+     // Invoke kernel
+    SumArrays<<<dimGrid, dimBlock>>>(d_array1, d_array2, d_out, values_per_thread);
+    error = cudaGetLastError();
+    if (error != cudaSuccess) Cleanup(false);
+    cudaDeviceSynchronize();
+
+    auto t3 = high_resolution_clock::now();
+    result.calc_time = duration_cast<microseconds>(t3-t2);
+    
+    error = cudaMemcpy(h_out, d_out, count * sizeof(float), cudaMemcpyDeviceToHost);
+    if (error != cudaSuccess) Cleanup(false);
+    cudaDeviceSynchronize();
+
+    // This is a bit pointless here
+    if (!verify_sum_arrays(count, h_array1, h_array2, h_out)) {
+        cout << "Verification failed!" << endl;
+        Cleanup(false);
+    }
+
+    result.last_sum = h_out[count-1];
+
+    if (h_array1) free(h_array1);
+    if (h_array2) free(h_array2);
+    if (h_out) free(h_out);
+    if (d_array1) cudaFree(d_array1);
+    if (d_array2) cudaFree(d_array2);
+    if (d_out) cudaFree(d_out);
+
+    auto t4 = high_resolution_clock::now();
+    result.total_time = duration_cast<microseconds>(t4-t0);
+    return result;
+}
+
+void print_results(alloc_sum_result result) {
+    cout << "Total Time: " << duration_cast<milliseconds>(result.total_time).count() << "ms" << endl;
+    cout << "Allocation Time: " << duration_cast<milliseconds>(result.alloc_time).count() << "ms" << endl;
+    cout << "Populate Time: " << duration_cast<milliseconds>(result.populate_time).count() << "ms" << endl;
+    cout << "Calculation Time: " << duration_cast<microseconds>(result.calc_time).count() << "us" << endl;
+    cout << "Total Time: " << duration_cast<milliseconds>(result.total_time).count() << "ms" << endl;
+    cout << "Last Sum Value: " << result.last_sum << endl;
+
+}
+
+int main(int const argc, char *argv[]) {
+
+    if (argc != 2){
+        cout << "Usage: " << argv[0] << " ELEMS\n";
+        exit(1);
+    }
+
+    size_t elems;
+    stringstream sstream(argv[1]);
+    sstream >> elems;
+    if (elems == 0) {
+        cout << "Invalid element size\n";
+        exit(1);
+    }
+
+    cout << "Using array of size: " << elems << "M" << endl;
+    size_t blocks;
+    size_t threads;
+
+    elems *= 1e6;
+    // Must be multiple of largest threads (256)
+    elems = ((elems + 255) / 256) * 256;
+
+    blocks = 1;
+    threads = 1;
+    cout << "\nRunning test with " << blocks << " blocks/grid and " << threads << " threads per block\n";
+    auto result = time_sum_arrays(elems, blocks, threads);
+    print_results(result);
+   
+
+    blocks = 1;
+    threads = 256;
+    cout << "\nRunning test with " << blocks << " blocks/grid and " << threads << " threads per block\n";
+    result = time_sum_arrays(elems, blocks, threads);
+    print_results(result);
+
+    threads = 256;
+    blocks = elems / threads;
+    cout << "\nRunning test with " << blocks << " blocks/grid and " << threads << " threads per block\n";
+    result = time_sum_arrays(elems, blocks, threads);
+    print_results(result);
+
+
+
+}
