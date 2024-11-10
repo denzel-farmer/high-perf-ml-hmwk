4c832eccf682e942536ba3f3c4a79b049dcbd304
Denzel Farmer <denzelgfarmer@gmail.com>
2024-11-10 00:31:55 +0000

    Most of part b

--- hmwk3/CUDA1/Part-B/q3.cu
+++ hmwk3/CUDA1/Part-B/q3.cu
@@ -36,8 +36,6 @@ __global__ void SumArrays(const float* A, const float* B, float* C, int N)
     int blockStartIndex  = blockIdx.x * blockDim.x;
     int threadStartIndex = blockStartIndex + threadIdx.x;
 
-    int thread_id = (blockIdx.x)*(blockDim.x) + (threadIdx.x);
-
     int i, curr_index;
     
     for (i=0; i < N; i++) {
@@ -48,25 +46,18 @@ __global__ void SumArrays(const float* A, const float* B, float* C, int N)
 }
 
 
-float *h_array1, *d_array1, *h_array2, *d_array2, *h_out, *d_out;
+float *dh_array1, *dh_array2, *dh_out;
 
 void Cleanup(bool noError) {  // simplified version from CUDA SDK
     cudaError_t error;
     // Free device vectors
-    if (d_array1)
-        cudaFree(d_array1);
-    if (d_array2)
-        cudaFree(d_array2);
-    if (d_out)
-        cudaFree(d_out);
+    if (dh_array1)
+        cudaFree(dh_array1);
+    if (dh_array2)
+        cudaFree(dh_array2);
+    if (dh_out)
+        cudaFree(dh_out);
 
-    // Free host memory
-    if (h_array1)
-        free(h_array1);
-    if (h_array2)
-        free(h_array2);
-    if (h_out)
-        free(h_out);
     error = cudaDeviceReset();
     
     if (!noError || error != cudaSuccess)
@@ -96,35 +87,31 @@ alloc_sum_result time_sum_arrays(size_t count, size_t blocks, size_t threads) {
     result.calc_time = microseconds(-1);
     result.total_time = microseconds(-1);
     auto t0 = high_resolution_clock::now();
-    
-    h_array1 = (float *)malloc(count * sizeof(float));
-    if (!h_array1) Cleanup(false);
-    h_array2 = (float *)malloc(count * sizeof(float));
-    if (!h_array2) Cleanup(false);
-    h_out = (float *)malloc(count * sizeof(float));
-    if (!h_out) Cleanup(false);
 
     cudaError_t error;
-    error = cudaMalloc((void**)&d_array1, count * sizeof(float));
+    error = cudaMallocManaged((void**)&dh_array1, count * sizeof(float));
     if (error != cudaSuccess) Cleanup(false);
-    error = cudaMalloc((void**)&d_array2, count * sizeof(float));
+    error = cudaMallocManaged((void**)&dh_array2, count * sizeof(float));
     if (error != cudaSuccess) Cleanup(false);
-    error = cudaMalloc((void**)&d_out, count * sizeof(float));
+    error = cudaMallocManaged((void**)&dh_out, count * sizeof(float));
     if (error != cudaSuccess) Cleanup(false);
 
     auto t1 = high_resolution_clock::now();
     result.alloc_time = duration_cast<microseconds>(t1 - t0);
 
     for (size_t i = 0; i < count; i++) {
-        h_array1[i] = (float) i;
-        h_array2[i] = (float) count - i;
+        dh_array1[i] = (float) i;
+        dh_array2[i] = (float) count - i;
     }
 
-    // Copy to device 
-    error = cudaMemcpy(d_array1, h_array1, count*sizeof(float), cudaMemcpyHostToDevice);
-    if (error != cudaSuccess) Cleanup(false);
-    error = cudaMemcpy(d_array2, h_array2, count*sizeof(float), cudaMemcpyHostToDevice);
-    if (error != cudaSuccess) Cleanup(false);
+
+    int device = -1;
+    cudaGetDevice(&device);
+    cudaMemPrefetchAsync(dh_array1, count*sizeof(float), device, NULL);
+    cudaMemPrefetchAsync(dh_array2, count*sizeof(float), device, NULL);
+    cudaMemPrefetchAsync(dh_out, count*sizeof(float), device, NULL);
+    
+
 
     auto t2 = high_resolution_clock::now();
     result.populate_time = duration_cast<microseconds>(t2 - t1);
@@ -135,7 +122,7 @@ alloc_sum_result time_sum_arrays(size_t count, size_t blocks, size_t threads) {
     size_t values_per_thread = (count) / (blocks * threads);
 
      // Invoke kernel
-    SumArrays<<<dimGrid, dimBlock>>>(d_array1, d_array2, d_out, values_per_thread);
+    SumArrays<<<dimGrid, dimBlock>>>(dh_array1, dh_array2, dh_out, values_per_thread);
     error = cudaGetLastError();
     if (error != cudaSuccess) Cleanup(false);
     cudaDeviceSynchronize();
@@ -143,24 +130,17 @@ alloc_sum_result time_sum_arrays(size_t count, size_t blocks, size_t threads) {
     auto t3 = high_resolution_clock::now();
     result.calc_time = duration_cast<microseconds>(t3-t2);
     
-    error = cudaMemcpy(h_out, d_out, count * sizeof(float), cudaMemcpyDeviceToHost);
-    if (error != cudaSuccess) Cleanup(false);
-    cudaDeviceSynchronize();
-
     // This is a bit pointless here
-    if (!verify_sum_arrays(count, h_array1, h_array2, h_out)) {
+    if (!verify_sum_arrays(count, dh_array1, dh_array2, dh_out)) {
         cout << "Verification failed!" << endl;
         Cleanup(false);
     }
 
-    result.last_sum = h_out[count-1];
+    result.last_sum = dh_out[count-1];
 
-    if (h_array1) free(h_array1);
-    if (h_array2) free(h_array2);
-    if (h_out) free(h_out);
-    if (d_array1) cudaFree(d_array1);
-    if (d_array2) cudaFree(d_array2);
-    if (d_out) cudaFree(d_out);
+    if (dh_array1) cudaFree(dh_array1);
+    if (dh_array2) cudaFree(dh_array2);
+    if (dh_out) cudaFree(dh_out);
 
     auto t4 = high_resolution_clock::now();
     result.total_time = duration_cast<microseconds>(t4-t0);
@@ -214,7 +194,7 @@ int main(int const argc, char *argv[]) {
     print_results(result);
 
     threads = 256;
-    blocks = elems / 256;
+    blocks = elems / threads;
     cout << "\nRunning test with " << blocks << " blocks/grid and " << threads << " threads per block\n";
     result = time_sum_arrays(elems, blocks, threads);
     print_results(result);
